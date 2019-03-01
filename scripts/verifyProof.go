0f9c4e25a07a8bac4be953ccc60fc65213562ef8
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2019-03-01 11:37:40 +0800

    update comments
    update comments

--- scripts/verifyProof.go
+++ scripts/verifyProof.go
@@ -21,18 +21,18 @@ var (
 	headerPrefix = []byte("h")
 )
 
-//Hash byte-Array's length
+// Hash byte-Array's length
 const HashLength = 32
 
-//char maps to bit integer
+// char maps to bit integer
 var hexmaps [128]uint64
 
-//common.Hash
+// common.Hash
 type Hash [HashLength]byte
 
 func (h Hash) bytes() []byte { return h[ : ] }
 
-//TODO - iterator
+// TODO - iterator
 func checkAll(db *leveldb.DB){
 	iter := db.NewIterator(nil, nil)
 	for iter.Next(){
@@ -42,7 +42,7 @@ func checkAll(db *leveldb.DB){
 	}
 }
 
-//unint64(8-bytes) to bytes
+// unint64(8-bytes) to bytes
 func uint64toslice(number uint64) []byte {
 	enc := make([]byte, 8)
 	for idx := uint64(0); idx < 8; idx++ {
@@ -56,9 +56,11 @@ func uint64toslice(number uint64) []byte {
 func stringtohash(hashstr string) Hash {
 	var hres Hash
 	ofs := uint64(0)
+
 	if hashstr[1] == 'x' {
 		ofs = 1
 	}
+
 	for idx	:= uint64(0); idx < 32; idx++ {
 		hres[idx] |= byte(hexmaps[hashstr[ (idx + ofs) << 1     ]] << 4)
 		hres[idx] |= byte(hexmaps[hashstr[((idx + ofs) << 1) | 1]])
@@ -66,50 +68,140 @@ func stringtohash(hashstr string) Hash {
 	return hres
 }
 
-//input a string and return common.Hash
+//input a string and return a byte slice
 func stringtobytes(bytes string) []byte {
 	glen := len(bytes)
 	if  glen <= 1 || ((glen & 1) == 1) {
 		return nil
 	}
+
 	glen >>= 1
-	bres ,ofs := make([]byte, glen, glen), 0
+	ofs := 0
+
 	if bytes[1] == 'x' {
-		ofs = 2
+		ofs = 1
 	}
-	for idx := int(ofs); idx < glen; idx++ {
+
+	glen -= ofs
+
+	bres := make([]byte, glen, glen)
+	for idx := 0; idx < glen; idx++ {
 		bres[idx] |= byte(hexmaps[bytes[(idx + ofs) << 1 ]] << 4)
 		bres[idx] |= byte(hexmaps[bytes[(idx + ofs) << 1 | 1]])
 	}
 	return bres
 }
 
-func stringtonibbles(nibbles string) []bytes {
+//input a string and return a nibble slice
+func stringtonibbles(nibbles string) []byte {
 	glen := len(nibbles)
-	bres ,ofs := make([]byte, glen, glen), 0
+
+	ofs := 0
 	if nibbles[1] == 'x' {
 		ofs = 2
 	}
-	for idx := int(ofs); idx < glen; idx++ {
-		bres[idx] |= byte(hexmaps[nibbles[(idx + ofs)]])
+	glen -= ofs
+
+	bres := make([]byte, glen, glen)
+	for idx := 0; idx < glen; idx++ {
+		bres[idx] = byte(hexmaps[nibbles[idx + ofs]])
 	}
+	return bres
 }
 
+// input a block number and the corresponding block hash, return a headerkey
 func stringtoheaderkey(number uint64, hashstr string) []byte {
 	return append(append(headerPrefix, uint64toslice(number)...), stringtohash(hashstr).bytes()...);
 }
 
-func Cook(db *leveldb.DB, query []byte) ([]byte, error) {
-	binfo, err := db.Get(query, nil)
-	if err == nil {
-		// fmt.Printf("[%x]\n",binfo)
-		// fmt.Println("type:",reflect.TypeOf(binfo))
-		// fmt.Printf("%x",binfo)
-		// rlp.PrintListInString(rlp.Unserialize(binfo))
-		return binfo, nil
+// compare two byte slice
+func bytesequal(nib1 []byte, nib2 []byte) bool {
+	len1, len2 := len(nib1), len(nib2)
+	if len1 != len2 {
+		return false
+	}
+	for idx := 0; idx < len1 ; idx++ {
+		if nib1[idx] != nib2[idx] {
+			return false
+		}
+	}
+	return true
+}
+
+// get value of key on the trie
+func findPath(db *leveldb.DB, rootHashStr *string, path string, storagepath []string, consumed int) (string ,error) {
+
+	//key consumed
+	if len(path) == 0 {
+		return "", errors.New("No exists")
+	}
+
+	// get node from db
+	querynode, err := db.Get(stringtohash(*rootHashStr).bytes(), nil)
+	if err != nil {
+		return "", err;
 	}else {
-		fmt.Println("query error!", err)
-		return nil, err
+
+		// compare to storagepath
+		if hex.EncodeToString(querynode) != storagepath[0] {
+			return "", errors.New("No exists")
+		}
+
+		node := rlp.Unserialize(querynode)
+
+		switch node.Length() {
+			case 2: {
+				firstvar, secondvar := node.Get(0).AsString(), node.Get(1).AsString()
+
+				//end of proofpath
+				if len(storagepath) == 1 {
+					if path != firstvar[consumed:] {
+						return "", errors.New("No exists")
+					}
+					return secondvar, nil
+				}
+
+				//compare prefix of the path with node[0]
+				firstvarlen := len(firstvar)
+				if (firstvarlen > len(path)) || (firstvar != path[0:firstvarlen]) {
+					return "", errors.New("No exists")
+				}
+
+				return findPath(db, &secondvar, path[firstvarlen: ], storagepath[1 : ], consumed + firstvarlen)
+			}
+			case 17: {
+				tryquery := node.Get(int(hexmaps[path[0]])).AsString()
+
+				if len(tryquery) == 64 {
+					return findPath(db, &tryquery, path[1 : ], storagepath[1 : ], consumed + 1)
+				}else {
+					return "", errors.New("No exists")
+				}
+			}
+			default: {
+				return "", errors.New("Unknown node types")
+			}
+		}
+	}
+}
+
+func VerifyProof(db *leveldb.DB, rootHashStr string, key string, value string, storagepath []string) {
+	if key[0:2] == "0x" {
+		key = key[2:]
+	}
+	if value[0:2] == "0x" {
+		value = value[2:]
+	}
+	// key = append(make([]byte,0),2,9)
+	toval, err := findPath(db, &rootHashStr, key, storagepath, 0)
+	if err != nil {
+		fmt.Println(err)
+	}else {
+		if value == toval {
+			fmt.Println("Proved")
+		}else {
+			fmt.Println("key maps to", "0x" + toval + ", not", "0x" + value)
+		}
 	}
 }
 
@@ -124,70 +216,20 @@ func init() {
 		hexmaps[idx] = uint64(idx - 'A' + 10)
 	}
 }
-//2084db4a68aa8b172f70bc04e2e74541617c003374de6eb4b295e823e5beab01
-//200decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563
-//                 1
-func findPath(db *leveldb.DB, rootHashStr *string, path []byte, storagepath []string, consumed uint32) ([]byte ,error) {
-	roothash := stringtohash(*rootHashStr)
-	querynode, err := Cook(db, roothash[ : ])
-	if err != nil {
-		return nil, err;
-	}else {
-		if hex.EncodeToString(querynode) != storagepath[0] {
-			return nil, errors.New("No exists")
-		}
-		node := rlp.Unserialize(querynode)
-		switch node.Length() {
-			case 2: {
-				firstvar, secondvar := node.Get(0).AsString(), node.Get(1).AsString()
-				fmt.Println("Test", consumed, firstvar, secondvar)
-				if len(path) == 30 {
-					if hex.EncodeToString(path) != firstvar[consumed:] {
-						return nil, errors.New("No exists")
-					}
-					return querynode, nil
-				}
-				return findPath(db, &secondvar, path[1 : ], storagepath[1 : ], consumed + 1)
-			}
-			case 17: {
-				tryquery := node.Get(int(path[0])).AsString()
-				if len(tryquery) == 64 {
-					return findPath(db,&tryquery,path[1 : ], storagepath[1 : ], consumed + 1)
-				}else{
-					return nil, errors.New("No exists")
-				}
-			}
-			default: {
-				err := errors.New("Unknown node types")
-				return nil, err
-			}
-		}
-	}
-}
 
-func VerifyProof(db *leveldb.DB, rootHashStr *string, keyvalue *string, storagepath []string) {
-	keyvaluelist := rlp.Unserialize(stringtobytes(*keyvalue))
-	key, value := keyvaluelist.Get(0).AsString(), keyvaluelist.Get(1).AsString()
-	fmt.Println(len(key))
-	// key = append(make([]byte,0),2,9)
-	toval, err := findPath(db, rootHashStr, stringtonibbles(key), storagepath, 0)
-	fmt.Println("finally", toval, value, err)
-	rlp.PrintListInString(rlp.Unserialize(toval))
-}
 func main(){
-	//fmt.Println(stringtohash("6fb589682cc8db511886024d278b6712b4d886db988b5692e261ed4c9d709f8e").bytes())
 	StorageHash := "0x11e91152ab237ceff29728c03999ef2debadd7db0fc45b280657c6f7cc4c1ffa"
-	StorageProofPathVal := "e2a0200decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e56333"
 	StoragePath := []string{"f8918080a06d032ff808e3a2b585df339916901d7b7d04c5bd18a088607093d3178172b7ee8080808080a0071b011fdbd4ad7d1e6f9762be4d1a88dffde614a6bd399bf3b5bad8f41249b5808080a01b56cc0a5b9b1ce34e9a14e896ea000c830bd64387573d238cbe3fa24ddfa2c3a0f5c2efa606e3a5be341f22bf1d5c8f4bce679719870c097a24abb38aec0a4855808080", "f8518080808080a06f643b8fd2176a403e2ccfae43808c4543289e1082078e91d821d1c7886d6f51808080a03822ab26403807d175522401e184b20b5aa8c7fcd802f4793970a70e810f4ce980808080808080", "e2a0200decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e56333"}
-	//StoragePath := append(make([]byte,0),2,9)
-	
+
+	key := "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
+	val := "0x33"
 	
 	db, err := leveldb.OpenFile(EDB_PATH, nil)
-	if err != nil{
+	if err != nil {
 		fmt.Println("link error")
 		fmt.Println(err)
 	}else {
-		VerifyProof(db, &StorageHash, &StorageProofPathVal, StoragePath)
+		VerifyProof(db, StorageHash, key, val, StoragePath)
 		db.Close()
 	}
 }
