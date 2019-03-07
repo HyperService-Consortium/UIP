539cb732220ac824c12e9c166f16e9b40ed7e6fa
Myriad-Dreamin <camiyoru@gmail.com>
2019-03-07 13:15:39 +0800

    README

--- uiputils/cast.py
+++ uiputils/cast.py
@@ -3,26 +3,31 @@
 
 
 def uintxstring(num, x):
+	# return x-bit string of num
     nums = str(num)
     return "0" * (x - len(nums)) + nums
 
 
 def uint32string(num):
+	# return 32-bit string of num
     nums = str(num)
     return "0" * (32 - len(nums)) + nums
 
 
 def uint64string(num):
+	# return 64-bit string of num
     nums = str(num)
     return "0" * (64 - len(nums)) + nums
 
 
 def uint128string(num):
+	# return 128-bit string of num
     nums = str(num)
     return "0" * (128 - len(nums)) + nums
 
 
 def uint256string(num):
+	# return 256-bit string of num
     nums = str(num)
     return "0" * (256 - len(nums)) + nums
 
--- uiputils/gotypes.py
+++ uiputils/gotypes.py
@@ -1,11 +1,14 @@
 
 import ctypes
 
+
+# GoInt in C
 GoInt8 = ctypes.c_int8
 GoInt16 = ctypes.c_int16
 GoInt32 = ctypes.c_int32
 GoInt64 = ctypes.c_int64
 
+# GoUint in C
 GoUint8 = ctypes.c_uint8
 GoUint16 = ctypes.c_uint16
 GoUint32 = ctypes.c_uint32
@@ -14,6 +17,7 @@ GoUint64 = ctypes.c_uint64
 GoInt = GoUint64
 GoUInt = GoUint64
 
+# the integer coresponds to the GolevelDB-array's index
 GolevelDBptr = ctypes.c_int
 
 
