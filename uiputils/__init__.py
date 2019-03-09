abef99aa2423418ffdecaaca087f24b2bdd0b144
Myriad-Dreamin <camiyoru@gmail.com>
2019-03-09 16:59:26 +0800

    new method of calculating location

--- uiputils/__init__.py
+++ uiputils/__init__.py
@@ -3,3 +3,7 @@
 __author__ = 'Myriad Dreamin'
 
 from .uiptypes import BlockchainNetwork, SmartContract, StateProof
+
+import os.path as parsepath
+
+INCLUDE_PATH = parsepath.dirname(__file__) + "/include"
--- uiputils/eth/tools.py
+++ uiputils/eth/tools.py
@@ -1,13 +1,20 @@
 from ctypes import CDLL
 from uiputils.gotypes import GoString, GoInt32, GoStringSlice, GolevelDBptr
 from eth_hash.auto import keccak
-from uiputils.cast import fillbytes32, catbytes32
+from uiputils.cast import fillbytes32, catbytes32, bytestoint, fillint32, transbytes32, transint
+from uiputils import INCLUDE_PATH
+from hexbytes import HexBytes
+# from sys import path as loadpathlist
+from os import path as parsepath
+# if pathofinclude not in loadpathlist:
+#     loadpathlist.append(pathofinclude)
+# print("\n".join(loadpathlist))
 
-PROVER_PATH = "./uiputils/include/verifyproof.dll"
+
 ENC = "utf-8"
 MOD256 = (1 << 256) - 1
 
-funcs = CDLL(PROVER_PATH)
+funcs = CDLL(INCLUDE_PATH + "/verifyproof.dll")
 
 funcs.openDB.restype = GolevelDBptr
 funcs.openDB.argtype = GoString.Type
@@ -43,17 +50,34 @@ class Prover:
         funcs.VerifyProof(self.ethdb, hashptr, keyptr, valptr, path, len(storagepath))
 
 
-def slicelocation32(bytesslot, index, element_size):
-    return catbytes32(bytes((int(keccak(bytesslot)) + (index * element_size))))
+def sliceloc32(bytesslot, index, element_size):
+    return catbytes32(fillint32(bytestoint(keccak(bytesslot)) + (index * element_size)))
+
+
+def sliceloc(bytesslot, index, element_size):
+    return catbytes32(fillint32(bytestoint(keccak(fillbytes32(bytesslot))) + (index * element_size)))
 
 
 def slicelocation(slot, index, element_size):
-    return catbytes32(bytes(int(keccak(fillbytes32(slot))) + (index * element_size)))
+    return sliceloc32(transbytes32(slot), transint(index), element_size)
 
 
-def maplocation64(bytesslot, byteskey):
+def maploc32(bytesslot, byteskey):
     return keccak(bytesslot + byteskey)
 
 
+def maploc(bytesslot, byteskey):
+    return keccak(catbytes32(bytesslot) + catbytes32(byteskey))
+
+
+def maplocation(slot, key):
+    return keccak(transbytes32(key) + transbytes32(slot))
+
+
 if __name__ == '__main__':
-    print(slicelocation())
+    print(HexBytes(sliceloc(b'\x01', 1, 8)).hex())
+    print(HexBytes(slicelocation(b'\x01', 1, 8)).hex())
+    print(HexBytes(slicelocation(1, 1, 16)).hex())
+    print(HexBytes(slicelocation("1", 1, 8)).hex())
+    print(HexBytes(slicelocation("0x1", 1, 8)).hex())
+    print(HexBytes(maploc(b'\x00', b'\x00')).hex())
