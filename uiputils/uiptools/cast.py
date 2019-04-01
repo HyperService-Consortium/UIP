f4b04c7674157f0fbd8daa843b382090e8a81928
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2019-04-01 11:32:30 +0800

    uiptools

--- uiputils/uiptools/cast.py
+++ uiputils/uiptools/cast.py
@@ -1,5 +1,5 @@
 
-'some cast methods'
+"""some cast methods"""
 
 # python modules
 import json
@@ -10,7 +10,7 @@ import rlp
 from hexbytes import HexBytes
 
 # uip modules
-from uiputils.uiperror import DecodeFail
+from uiputils.errors import DecodeFail
 
 # constant
 MOD512 = (1 << 512) - 1
